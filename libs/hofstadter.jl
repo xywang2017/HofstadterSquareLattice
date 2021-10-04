44d49b5bb09822093dfb119b912524c9bb351951
Xiaoyu Wang <wxybkaphsc@gmail.com>
2021-10-04 00:41:47 -0400

    base camp

--- libs/hofstadter.jl
+++ libs/hofstadter.jl
@@ -26,7 +26,7 @@ mutable struct Hofstadter
     Hofstadter() = new()
 end
 
-function computeHofstadter(;p::Int=7,q::Int=64,ndim::Int=9)
+function computeHofstadter(;p::Int=7,q::Int=64,ndim::Int=5)
     lk = q # along k2 direction, different values of l1 decouple
     A = initHamiltonian(lk=q,V0=3.0)
     
@@ -48,7 +48,7 @@ function computeHofstadter(;p::Int=7,q::Int=64,ndim::Int=9)
     B.O2 = zeros(Float64,B.l1,B.l1,B.l2,ndim)
     B.O3 = zeros(Float64,B.l1,B.l1,B.l2,ndim)
 
-    gq = -(A.lg-1)÷2 :(A.lg-1)÷2
+    gq = (-(A.lg-1)÷2) :(A.lg-1)÷2
     k1rc = reshape(B.k1,:,1) .- reshape(B.k1,1,:)
 
     # Fourier transform of x/L1x, multiply by 1im at the end
@@ -73,15 +73,13 @@ function computeHofstadter(;p::Int=7,q::Int=64,ndim::Int=9)
                 i1c = i1r
                 ur = A.Uk[:,1,i1r,i2r]
                 uc = A.Uk[:,1,i1c,i2c]
-                # ur = reshape(circshift(reshape(ur,A.lg,A.lg),(0,δg2[i2r])),A.lg^2)
                 uc = reshape(uc,A.lg,A.lg)
-                # uc = reshape(circshift(uc,(0,-δg2[i2r])),A.lg^2)
-                uc = reshape(circshift(uc,(0,0)),A.lg^2)
+                uc = reshape(circshift(uc,(0,-δg2[i2r])),A.lg^2)
                 
                 λrc = ur' * uc
                 B.O0[i1r,i1c,i2r,n] = λrc 
                 B.O1[i1r,i1c,i2r,n] = A.Hk[1,i1r,i2r]*λrc
-                # B.O1[i1r,i1c,i2r,n] = A.Hk[1,i1c,i2c]*λrc
+
             end
         end
 
@@ -108,7 +106,7 @@ function computeHofstadter(;p::Int=7,q::Int=64,ndim::Int=9)
     for i1 in eachindex(B.k1)
         for n in eachindex(B.nvec)
             θc = reshape(exp.(-1im * 2π * B.k1 * B.nvec[n]),1,B.l1)
-            θϕ = exp(1im * 2π * B.nvec[n]* B.k1[i1])
+            θϕ = exp(1im * 2π * B.nvec[n]* B.k1[i1] )
             qϕ2 = B.nvec[n] * B.δq2
             tmp = eachindex(B.k2) .- qϕ2   # split into [k1-q1] + δg
             k2c = mod.(tmp .-1,B.l2) .+ 1
@@ -128,9 +126,10 @@ function computeHofstadter(;p::Int=7,q::Int=64,ndim::Int=9)
     for i1 in eachindex(B.k1)
         M = B.M[:,:,i1]
         H = B.H[:,:,i1]
-        F = eigen(M)
-        U = Diagonal(1 ./ sqrt.(F.values)) * F.vectors'
-        Hnew = U * H * U'
+        F = eigen(Hermitian(M))
+
+        U = F.vectors * Diagonal(1 ./ sqrt.(F.values))
+        Hnew = U' * H * U
         F = eigen(Hermitian(Hnew))
         B.spectrum[:,i1] = F.values
     end
